d27648d970d3887a2b8d0174bb9d91cbd385d8b3
wjeon <novajeon@yahoo.com>
2017-01-02 03:05:57 -0800

    GWT-0002 Added more tests

--- TinyGivenWhenThenParser.Tests.Unit/TinyGWTParserTests.cs
+++ TinyGivenWhenThenParser.Tests.Unit/TinyGWTParserTests.cs
@@ -22,5 +22,39 @@ namespace TinyGivenWhenThenParser.Tests.Unit
 
             parseResult.ShouldAllBeEquivalentTo(expectedData);
         }
+
+        [TestCase(@"When Tom eats 1 apple and 2 oranges", "Tom,1, apple,, and ,2, orange,s")]
+        [TestCase(@"When Jerry eats 1 orange", "Jerry,,,,,1, orange,")]
+        [TestCase(@"Given Jerry eats 1 orange", "")]
+        public void Parse_data_from_a_sentence_correctly_with_the_pattern_for_When_when_the_sentence_starts_with_When
+            (string @case, string parsedData)
+        {
+            var gwtParser = TinyGWTParser.WithTestCase(@case);
+
+            const string pattern = @"^When (.*) eats (|\d+)( apple|)(s|)(| and )(|\d+)( orange|)(s|)$";
+
+            var parseResult = gwtParser.WithPattern(pattern).ParseData();
+
+            var expectedData = string.IsNullOrEmpty(parsedData) ? new string[0] : parsedData.Split(',');
+
+            parseResult.ShouldAllBeEquivalentTo(expectedData);
+        }
+
+        [TestCase(@"Then Tom has 1 apple and 1 orange", "Tom,1, apple,, and ,1, orange,")]
+        [TestCase(@"Then Jerry has 1 apple", "Jerry,1, apple,,,,,")]
+        [TestCase(@"Given Jerry has 1 apple", "")]
+        public void Parse_data_from_a_sentence_correctly_with_the_pattern_for_Then_when_the_sentence_starts_with_Then
+            (string @case, string parsedData)
+        {
+            var gwtParser = TinyGWTParser.WithTestCase(@case);
+
+            const string pattern = @"^Then (.*) has (|\d+)( apple|)(s|)(| and )(|\d+)( orange|)(s|)$";
+
+            var parseResult = gwtParser.WithPattern(pattern).ParseData();
+
+            var expectedData = string.IsNullOrEmpty(parsedData) ? new string[0] : parsedData.Split(',');
+
+            parseResult.ShouldAllBeEquivalentTo(expectedData);
+        }
     }
 }
