e8e0395e26fc1ad1774eb2c938c3737d2a032e20
wjeon <novajeon@yahoo.com>
2017-01-01 20:58:33 -0800

    GWT-0001 Added and implemented TinyGWTParser to pass unit tests

---
+++ TinyGivenWhenThenParser/TinyGWTParser.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace TinyGivenWhenThenParser
+{
+    public class TinyGWTParser
+    {
+        public IList<string> ParseData(string value, string pattern)
+        {
+            var matchResult = Regex.Match(value, pattern);
+
+            if (!matchResult.Success)
+                return new List<string>();
+
+            var result = new List<string>();
+            for (var i = 1; i < matchResult.Groups.Count; i++)
+            {
+                result.Add(matchResult.Groups[i].Value);
+            }
+            return result;
+        }
+    }
+}
