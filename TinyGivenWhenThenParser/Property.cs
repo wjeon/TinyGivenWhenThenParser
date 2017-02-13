24eeb4bf753a512c2e0e9338bb828327dc42e4a4
wjeon <novajeon@yahoo.com>
2017-02-13 06:46:33 -0800

    GWT-0007 Added Property class

---
+++ TinyGivenWhenThenParser/Property.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TinyGivenWhenThenParser
+{
+    public class Property
+    {
+        public Property(string name, Type type)
+        {
+            Name = name;
+            Type = type;
+        }
+
+        public string Name { get; private set; }
+        public Type Type { get; private set; }
+    }
+}
