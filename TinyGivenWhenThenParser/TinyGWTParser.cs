1ffbe108b5644660202a32d956c132f623cc02cb
wjeon <novajeon@yahoo.com>
2017-02-13 06:53:57 -0800

    GWT-0007 Added To method in TinyGWTParser

--- TinyGivenWhenThenParser/TinyGWTParser.cs
+++ TinyGivenWhenThenParser/TinyGWTParser.cs
@@ -116,5 +116,10 @@ namespace TinyGivenWhenThenParser
             }
             return gwtLines;
         }
+
+        public TinyGWTDynamicParser To(params Property[] properties)
+        {
+            return new TinyGWTDynamicParser();
+        }
     }
 }
