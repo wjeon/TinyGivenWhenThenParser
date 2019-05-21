36f97e5b11b579983503510efa605f99eacb2b41
Woo jun Jeon <wjeon@paybyphone.com>
2019-05-21 06:03:39 -0700

    GWT-015 Added table parsing logic in new StringListExtensions and StringTableExtensions classes

---
+++ TinyGivenWhenThenParser/Extensions/StringListExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace TinyGivenWhenThenParser.Extensions
+{
+    public static class StringListExtensions
+    {
+        public static bool HaveTableSource(this List<string> caseLineSegments)
+        {
+            return caseLineSegments.Count > 1;
+        }
+
+        public static IEnumerable<string> GetTableSource(this List<string> caseLineSegments)
+        {
+            return caseLineSegments.GetRange(1, caseLineSegments.Count - 1);
+        }
+    }
+}
---
+++ TinyGivenWhenThenParser/Extensions/StringTableExtensions.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TinyGivenWhenThenParser.Extensions
+{
+    public static class StringTableExtensions
+    {
+        public static IEnumerable<IEnumerable<string>> ToHorizontalRowsTable(this IEnumerable<string> tableSource)
+        {
+            var table = tableSource.Select(c => c.Replace("||", "|").Trim('|').Split('|').Select(v => v.Trim())).ToList();
+
+            var flippedTable = tableSource.First().Trim().EndsWith("||")
+                ? table
+                : table.FlipVerticalTableToHorizontal();
+
+            return flippedTable.GetRowsOnlyWithNoHeaders();
+        }
+
+        private static IEnumerable<IEnumerable<string>> FlipVerticalTableToHorizontal(this IEnumerable<IEnumerable<string>> table)
+        {
+            var flippedTable = new List<List<string>>();
+
+            foreach (var row in table)
+            {
+                for (var i = 0; i < row.Count(); i++)
+                {
+                    if (flippedTable.Count <= i)
+                    {
+                        flippedTable.Add(new List<string> { row.ToArray()[i] });
+                    }
+                    else
+                    {
+                        flippedTable[i].Add(row.ToArray()[i]);
+                    }
+                }
+            }
+
+            return flippedTable;
+        }
+
+        private static IEnumerable<IEnumerable<string>> GetRowsOnlyWithNoHeaders(this IEnumerable<IEnumerable<string>> table)
+        {
+            return table.ToList().GetRange(1, table.Count() - 1);
+        }
+    }
+}
