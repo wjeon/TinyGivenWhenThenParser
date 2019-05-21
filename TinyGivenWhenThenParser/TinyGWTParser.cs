6fc39eb66d88694e8558505feebd84ba32666eea
Woo jun Jeon <wjeon@paybyphone.com>
2019-05-21 06:10:00 -0700

    GWT-015 Changed TinyGWTParser to support table

--- TinyGivenWhenThenParser/TinyGWTParser.cs
+++ TinyGivenWhenThenParser/TinyGWTParser.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using TinyGivenWhenThenParser.Exceptions;
+using TinyGivenWhenThenParser.Extensions;
 using TinyGivenWhenThenParser.StringConverters;
 
 namespace TinyGivenWhenThenParser
@@ -12,97 +13,142 @@ namespace TinyGivenWhenThenParser
         private readonly IEnumerable<string> _testCaseLines;
         private readonly IEnumerable<string> _gwtLines;
         private string _pattern;
+        private const string TableLineBreak = "\v\t\a\r\b\f";
 
         private TinyGWTParser(string testCase)
         {
-            if (!testCase.StartsWith("Given ") && !testCase.StartsWith("When "))
+            var regex = new Regex("\r\n[ ]*\\|");
+            var replacedCase = regex.Replace(testCase, $"{TableLineBreak}|");
+
+            if (!replacedCase.StartsWith("Given ") && !replacedCase.StartsWith("When "))
                 throw new GwtParserException("Test case must start with 'Given' or 'When'");
 
-            _testCaseLines = testCase
-                .Split(new string[] { "\r\n", "\n" }, StringSplitOptions.None).Select(c => c.Trim());
+            _testCaseLines = replacedCase
+                .Split(new[] { "\r\n", "\n" }, StringSplitOptions.None).Select(c => c.Trim());
+
             _gwtLines = ToGwtLinesFrom(_testCaseLines);
         }
 
         public IList<Property> Properties { get; private set; }
 
-        public ParseResult<IList<string>> ParseSingleLine(From testCase = From.TestCaseReplacedAndWithGivenWhenThen)
+        public ParseResult<ParsedData<IList<string>, IEnumerable<IEnumerable<string>>>, IList<string>> ParseSingleLine(From testCase = From.TestCaseReplacedAndWithGivenWhenThen)
         {
             var result = ParseData(testCase, multiLine: false);
 
-            return new ParseResult<IList<string>>(result.Parsed, result.Data.Any() ? result.Data.First() : new List<string>());
+            return new ParseResult<ParsedData<IList<string>, IEnumerable<IEnumerable<string>>>, IList<string>>(
+                result.Parsed,
+                result.ParsedData != null && result.ParsedData.Any()
+                    ? result.ParsedData.First()
+                    : default(ParsedData<IList<string>, IEnumerable<IEnumerable<string>>>));
         }
 
-        public ParseResult<TReturn> ParseSingleLine<TParser, TReturn>(From testCase = From.TestCaseReplacedAndWithGivenWhenThen)
+        public ParseResults<IEnumerable<ParsedData<IList<string>, IEnumerable<IEnumerable<string>>>>, IList<string>> ParseMultiLines(From testCase = From.TestCaseReplacedAndWithGivenWhenThen)
+        {
+            var result = ParseData(testCase, multiLine: true);
+            return new ParseResults<IEnumerable<ParsedData<IList<string>, IEnumerable<IEnumerable<string>>>>, IList<string>>(result.Parsed, result.ParsedData);
+        }
+
+        public ParseResult<ParsedData<T, IEnumerable<IEnumerable<string>>>, T> ParseSingleLine<T>(From testCase = From.TestCaseReplacedAndWithGivenWhenThen)
+        {
+            var result = ParseData(testCase, multiLine: false);
+
+            return new ParseResult<ParsedData<T, IEnumerable<IEnumerable<string>>>, T>(
+                result.Parsed,
+                result.ParsedData != null && result.ParsedData.Any()
+                    ? new ParsedData<T, IEnumerable<IEnumerable<string>>>(
+                        result.ParsedData.First().Line != null && result.ParsedData.First().Line.Any()
+                            ? result.ParsedData.First().Line.ToCostruct<T>() : default(T),
+                            result.ParsedData.First().Table)
+                    : default(ParsedData<T, IEnumerable<IEnumerable<string>>>));
+        }
+
+        public ParseResults<IEnumerable<ParsedData<T, IEnumerable<IEnumerable<string>>>>, T> ParseMultiLines<T>(From testCase = From.TestCaseReplacedAndWithGivenWhenThen)
+        {
+            var result = ParseData(testCase, multiLine: true);
+
+            var dataListWithTable = new List<ParsedData<T, IEnumerable<IEnumerable<string>>>>();
+
+            if (result.ParsedData != null && result.ParsedData.Any())
+            {
+                dataListWithTable.AddRange(
+                        result.ParsedData.Select(
+                            d => new ParsedData<T, IEnumerable<IEnumerable<string>>>(
+                                d.Line != null && d.Line.Any() ? d.Line.ToCostruct<T>() : default(T),
+                                d.Table
+                            )
+                        )
+                    );
+            }
+
+            return new ParseResults<IEnumerable<ParsedData<T, IEnumerable<IEnumerable<string>>>>, T>(result.Parsed, dataListWithTable);
+        }
+
+        public ParseResult<ParsedData<TReturn, IEnumerable<IEnumerable<string>>>, TReturn> ParseSingleLine<TParser, TReturn>(From testCase = From.TestCaseReplacedAndWithGivenWhenThen)
             where TParser : IParser<TReturn>
         {
-            var parser = ParseSingleLine<TParser>(testCase);
+            var result = ParseSingleLine<TParser>(testCase);
 
-            return new ParseResult<TReturn>(parser.Parsed, parser.Data.Value);
+            return new ParseResult<ParsedData<TReturn, IEnumerable<IEnumerable<string>>>, TReturn>(
+                result.Parsed,
+                result.ParsedData == null
+                    ? null
+                    : new ParsedData<TReturn, IEnumerable<IEnumerable<string>>>(result.ParsedData.Line.Value, result.ParsedData.Table));
         }
 
-        public ParseResult<T> ParseSingleLine<T>(From testCase = From.TestCaseReplacedAndWithGivenWhenThen)
-        {
-            var result = ParseData(testCase, multiLine: false);
-
-            return new ParseResult<T>(result.Parsed,
-                result.Data.Any() ? result.Data.First().ToCostruct<T>() : default(T));
-        }
-
-        public ParseResult<IEnumerable<IList<string>>> ParseMultiLines(From testCase = From.TestCaseReplacedAndWithGivenWhenThen)
-        {
-            return ParseData(testCase, multiLine: true);
-        }
-
-        public ParseResult<IEnumerable<TReturn>> ParseMultiLines<TParser, TReturn>(From testCase = From.TestCaseReplacedAndWithGivenWhenThen)
+        public ParseResults<IEnumerable<ParsedData<TReturn, IEnumerable<IEnumerable<string>>>>, TReturn> ParseMultiLines<TParser, TReturn>(From testCase = From.TestCaseReplacedAndWithGivenWhenThen)
             where TParser : IParser<TReturn>
         {
             var parsers = ParseMultiLines<TParser>(testCase);
 
-            return new ParseResult<IEnumerable<TReturn>>(parsers.Parsed, parsers.Data.Select(e => e.Value));
+            return new ParseResults<IEnumerable<ParsedData<TReturn, IEnumerable<IEnumerable<string>>>>, TReturn>(
+                parsers.Parsed,
+                parsers.ParsedData == null
+                    ? EmptyParsedDataList<TReturn>()
+                    : parsers.ParsedData.Select(d =>
+                        new ParsedData<TReturn, IEnumerable<IEnumerable<string>>>(d.Line == null ? default(TReturn) : d.Line.Value, d.Table)));
         }
 
-        public ParseResult<IEnumerable<T>> ParseMultiLines<T>(From testCase = From.TestCaseReplacedAndWithGivenWhenThen)
+        private ParseResult<IEnumerable<ParsedData<IList<string>, IEnumerable<IEnumerable<string>>>>, IList<string>> ParseData(From testCase, bool multiLine)
         {
-            var result = ParseData(testCase, multiLine: true);
-
-            var dataList = new List<T>();
-
-            if (result.Data.Any())
-            {
-                dataList.AddRange(result.Data.Select(data => data.ToCostruct<T>()));
-            }
-
-            return new ParseResult<IEnumerable<T>>(result.Parsed, dataList);
-        }
-
-        private ParseResult<IEnumerable<IList<string>>> ParseData(From testCase, bool multiLine)
-        {
-            var lines = testCase == From.OriginalTestCase ? _testCaseLines : _gwtLines;
+            var caseLines = testCase == From.OriginalTestCase ? _testCaseLines : _gwtLines;
             var parsed = false;
-            var results = new List<IList<string>>();
+            var parsedData = new List<ParsedData<IList<string>, IEnumerable<IEnumerable<string>>>>();
 
-            foreach (var line in lines)
+            foreach (var caseLine in caseLines)
             {
-                var matchResult = Regex.Match(line, _pattern);
+                var caseLineSegments = caseLine.Split(new [] { TableLineBreak }, StringSplitOptions.None).ToList();
+
+                var matchResult = Regex.Match(caseLineSegments.First(), _pattern);
 
                 if (!matchResult.Success)
                     continue;
 
                 parsed = true;
 
-                var result = new List<string>();
+                var line = new List<string>();
                 for (var i = 1; i < matchResult.Groups.Count; i++)
                 {
-                    result.Add(matchResult.Groups[i].Value);
+                    line.Add(matchResult.Groups[i].Value);
                 }
 
-                results.Add(result);
+                if (caseLineSegments.HaveTableSource())
+                {
+                    var tableSource = caseLineSegments.GetTableSource();
+
+                    var table = tableSource.ToHorizontalRowsTable();
+
+                    parsedData.Add(new ParsedData<IList<string>, IEnumerable<IEnumerable<string>>>(line, table));
+                }
+                else
+                {
+                    parsedData.Add(new ParsedData<IList<string>, IEnumerable<IEnumerable<string>>>(line, EmptyTable()));
+                }
 
                 if (!multiLine)
                     break;
             }
 
-            return new ParseResult<IEnumerable<IList<string>>>(parsed, results);
+            return new ParseResult<IEnumerable<ParsedData<IList<string>, IEnumerable<IEnumerable<string>>>>, IList<string>>(parsed, parsedData);
         }
 
         public static TinyGWTParser WithTestCase(string testCase)
@@ -142,6 +188,16 @@ namespace TinyGivenWhenThenParser
             Properties = properties;
 
             return new TinyGWTDynamicParser(this);
+        }
+
+        private static IEnumerable<IEnumerable<string>> EmptyTable()
+        {
+            return Enumerable.Empty<IEnumerable<string>>();
+        }
+
+        private static IEnumerable<ParsedData<TReturn, IEnumerable<IEnumerable<string>>>> EmptyParsedDataList<TReturn>()
+        {
+            return Enumerable.Empty<ParsedData<TReturn, IEnumerable<IEnumerable<string>>>>();
         }
     }
 }
