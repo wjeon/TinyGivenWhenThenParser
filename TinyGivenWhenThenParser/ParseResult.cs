2cd7527fe873b50017fac2159e6f57d59c276679
Woo jun Jeon <wjeon@paybyphone.com>
2019-05-21 05:56:04 -0700

    GWT-015 Added ParsedData class and changed ParseResult class to support table parsing

--- TinyGivenWhenThenParser/ParseResult.cs
+++ TinyGivenWhenThenParser/ParseResult.cs
@@ -1,16 +1,57 @@
-﻿namespace TinyGivenWhenThenParser
-{
-    public class ParseResult<T>
-    {
-        public ParseResult() {}
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TinyGivenWhenThenParser.StringConverters;
 
-        public ParseResult(bool parsed, T data)
+namespace TinyGivenWhenThenParser
+{
+    public class ParseResult<TParsedData, TLine>
+    {
+        public ParseResult(bool parsed, TParsedData parsedData)
         {
             Parsed = parsed;
-            Data = data;
+            ParsedData = parsedData;
         }
 
         public bool Parsed { get; private set; }
-        public T Data { get; private set; }
+        public TParsedData ParsedData { get; private set; }
+
+        public ParseResult<ParsedData<TLine, IEnumerable<TTable>>, TLine> WithTableOf<TTable>()
+        {
+            if (typeof(TParsedData) == typeof(ParsedData<TLine, IEnumerable<TTable>>))
+            {
+                PrintTableIsAlreadyRequestedType();
+                return ParsedData as ParseResult<ParsedData<TLine, IEnumerable<TTable>>, TLine>;
+            }
+
+            if (typeof(TTable) == typeof(IEnumerable<IEnumerable<string>>))
+            {
+                PrintConversionToStringListNotNeeded();
+                return ParsedData as ParseResult<ParsedData<TLine, IEnumerable<TTable>>, TLine>;
+            }
+
+            var parsedData = ParsedData as ParsedData<TLine, IEnumerable<IEnumerable<string>>>;
+
+            var parsedDataWithConvertedTable = ParsedDataWithConvertedTable<TTable>(parsedData);
+
+            return new ParseResult<ParsedData<TLine, IEnumerable<TTable>>, TLine>(Parsed, parsedDataWithConvertedTable);
+        }
+
+        protected static void PrintTableIsAlreadyRequestedType()
+        {
+            Console.WriteLine("The table is already converted to the requested type");
+        }
+
+        protected static void PrintConversionToStringListNotNeeded()
+        {
+            Console.WriteLine("The table shouldn't need to be converted to string list rows");
+        }
+
+        protected static ParsedData<TLine, IEnumerable<TTable>> ParsedDataWithConvertedTable<TTable>(ParsedData<TLine, IEnumerable<IEnumerable<string>>> parsedData)
+        {
+            return new ParsedData<TLine, IEnumerable<TTable>>(
+                parsedData.Line,
+                parsedData.Table.Select(row => row.ToList().ToCostruct<TTable>()));
+        }
     }
 }
---
+++ TinyGivenWhenThenParser/ParsedData.cs
@@ -0,0 +1,15 @@
+﻿namespace TinyGivenWhenThenParser
+{
+    public class ParsedData<TLine, TTable>
+    {
+
+        public ParsedData(TLine line, TTable table)
+        {
+            Line = line;
+            Table = table;
+        }
+
+        public TLine Line { get; private set; }
+        public TTable Table { get; private set; }
+    }
+}
