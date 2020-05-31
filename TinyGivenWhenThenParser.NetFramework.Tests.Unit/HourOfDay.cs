b719a0046d6877dc2f843fe8f0b0f6f9730c78e4
wjeon <wjeon@users.noreply.github.com>
2020-05-31 14:04:25 -0700

    GWT-0016 Added .net freamework unit tests

---
+++ TinyGivenWhenThenParser.NetFramework.Tests.Unit/HourOfDay.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TinyGivenWhenThenParser.NetFramework.Tests.Unit
+{
+    public class HourOfDay
+    {
+        private readonly string _hourOfDay;
+
+        public HourOfDay(string value)
+        {
+            _hourOfDay = value;
+        }
+
+        public TimeSpan Value
+        {
+            get { return DateTime.Parse(_hourOfDay).TimeOfDay; }
+        }
+
+        public override string ToString()
+        {
+            return _hourOfDay;
+        }
+    }
+}
